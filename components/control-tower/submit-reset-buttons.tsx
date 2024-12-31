ada26f39f10c8cdd323d5b34d362cf7faa6738b7
akshayzeplinix <akshayp@301io.com>
2024-12-31 17:46:53 +0530

    feat: added  rules view based on selected period, rules payload to send

---
+++ components/control-tower/submit-reset-buttons.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+
+const SubmitResetButtons = ({ onSubmit, onReset, label }: { onSubmit: () => void; onReset: () => void; label: string }) => {
+    return (
+
+        <div className='flex justify-content-end gap-3 mt-1 p-1'>
+            <Button label="Reset" style={{ backgroundColor: "#ffff", color: "#DF177C", border: 'none' }} onClick={onReset} />
+            <Button label={label} style={{ backgroundColor: "#DF177C", border: 'none' }} onClick={onSubmit} />
+
+        </div>
+    );
+};
+
+export default SubmitResetButtons;
