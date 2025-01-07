95616307ac8002d8c0036a3a4a2b42632a3b1eb7
Abhishek Deshmukh <deshmukhabhishek7020@gmail.com>
2025-01-07 11:05:56 +0530

    taking pull

--- app/(full-page)/login/page.tsx
+++ app/(full-page)/login/page.tsx
@@ -40,14 +40,13 @@ const LoginPage = () => {
         }
 
         if (email && password) {
-
             setLoading(true);
 
             // const encryptedPassword = await encryptPassword(password);
-            
+
             // console.log(encryptedPassword);
-            
-            const response: any = await PostCall('/auth/sign-in', { email, password});
+
+            const response: any = await PostCall('/auth/sign-in', { email, password });
             setLoading(false);
             if (response.code == 'SUCCESS') {
                 console.log('login success');
@@ -55,8 +54,7 @@ const LoginPage = () => {
                 setUser(response.data);
                 setAuthToken(response.token);
                 setAuthData(response.token, response.refreshToken, response.data);
-                setUserDetails(response.data)
-
+                setUserDetails(response.data);
             } else if (response.code == 'RESET_PASSWORD') {
                 console.log('res', response);
                 setDisplayName(response.name);
@@ -76,9 +74,9 @@ const LoginPage = () => {
         e.preventDefault();
         const encryptedPassword = encryptPassword(password);
         navigator.clipboard.writeText(encryptedPassword).then(() => {
-          alert("Encrypted password copied to clipboard!");
+            alert('Encrypted password copied to clipboard!');
         });
-      };
+    };
 
     const containerClassName = classNames('surface-ground flex align-items-center justify-between min-h-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
 
@@ -124,7 +122,7 @@ const LoginPage = () => {
                                     Forgot your password?
                                 </Link>
                             </div>
-                            <InputText id="password" value={password} type="password" placeholder="Password" className="w-full mb-3" onChange={handlePassword} onCopy={handleCopy}/>
+                            <InputText id="password" value={password} type="password" placeholder="Password" className="w-full mb-3" onChange={handlePassword} onCopy={handleCopy} />
 
                             <div className="flex flex-wrap justify-content-left gap-3 mb-2">
                                 <div className="flex align-items-center mb-2">
