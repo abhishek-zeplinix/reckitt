ab673826b39d509f385ab5e66ccfcba9e31f3f4b
kaushal anand <kaushal.anand301io@gmail.com>
2025-01-21 18:23:46 +0530

    dialog delete added

--- app/(main)/manage-rules/page.tsx
+++ app/(main)/manage-rules/page.tsx
@@ -213,7 +213,7 @@ const ManageRulesPage = () => {
                         aria-label="Delete Rule"
                         className="bg-pink-500 border-pink-500 hover:text-white"
                         onClick={() => {
-                            openAllDeleteDialog(true);
+                            handleAllDelete()
                         }}
                         style={{ marginLeft: 10 }}
                     />
@@ -346,18 +346,33 @@ const ManageRulesPage = () => {
 
     const closeDeleteDialog = () => {
         setIsDeleteDialogVisible(false);
-    };
-    const openAllDeleteDialog = (items:any) => {
-        setIsAllDeleteDialogVisible(true);
-    };
-
-    const closeAllDeleteDialog = () => {
         setIsAllDeleteDialogVisible(false);
     };
-
+    const handleAllDelete = () => {
+        setIsAllDeleteDialogVisible(true);
+        setIsDeleteDialogVisible(true);
+    };
 
     const onDelete = async () => {
         setLoading(true);
+        if(isAllDeleteDialogVisible){
+            try {
+                const response = await DeleteCall(`/company/rules`);
+    
+                if (response.code === 'SUCCESS') {
+                    closeDeleteDialog();
+                    setAlert('success', 'Rule successfully deleted!');
+                    fetchData();
+                } else {
+                    setAlert('error', 'Something went wrong!');
+                    closeDeleteDialog();
+                }
+            } catch (error) {
+                setAlert('error', 'Something went wrong!');
+            } finally {
+                setLoading(false);
+            }
+        }else{
 
         try {
             const response = await DeleteCall(`/company/rules/${selectedRuleId}`);
@@ -375,28 +390,29 @@ const ManageRulesPage = () => {
         } finally {
             setLoading(false);
         }
+    }
     };
 
-    const BulkDelete = async () => {
-        setLoading(true);
+    // const BulkDelete = async () => {
+    //     setLoading(true);
 
-        try {
-            const response = await DeleteCall(`/company/rules/`);
+    //     try {
+    //         const response = await DeleteCall(`/company/rules/`);
 
-            if (response.code === 'SUCCESS') {
-                closeAllDeleteDialog();
-                fetchData();
-                setAlert('success', 'Rule successfully deleted!');
-            } else {
-                setAlert('error', 'Something went wrong!');
-                closeAllDeleteDialog();
-            }
-        } catch (error) {
-            setAlert('error', 'Something went wrong!');
-        } finally {
-            setLoading(false);
-        }
-    };
+    //         if (response.code === 'SUCCESS') {
+    //             closeDeleteDialog();
+    //             fetchData();
+    //             setAlert('success', 'Rule successfully deleted!');
+    //         } else {
+    //             setAlert('error', 'Something went wrong!');
+    //             closeDeleteDialog();
+    //         }
+    //     } catch (error) {
+    //         setAlert('error', 'Something went wrong!');
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // };
     return (
         <div className="grid">
             <div className="col-12">
@@ -531,33 +547,38 @@ const ManageRulesPage = () => {
                 </div>
 
                 <Dialog
-                                header="Delete confirmation"
-                                visible={isDeleteDialogVisible}
-                                style={{ width: layoutState.isMobile ? '90vw' : '35vw' }}
-                                className="delete-dialog"
-                                footer={
-                                    <div className="flex justify-content-center p-2">
-                                        <Button label="Cancel" style={{ color: '#DF177C' }} className="px-7" text onClick={closeDeleteDialog} />
-                                        <Button label="Delete" style={{ backgroundColor: '#DF177C', border: 'none' }} className="px-7 hover:text-white" onClick={onDelete} />
-                                    </div>
-                                }
-                                onHide={closeDeleteDialog}
-                            >
-                                {isLoading && (
-                                    <div className="center-pos">
-                                        <ProgressSpinner style={{ width: '50px', height: '50px' }} />
-                                    </div>
-                                )}
-                                <div className="flex flex-column w-full surface-border p-3 text-center gap-4">
-                                    <i className="pi pi-info-circle text-6xl" style={{ marginRight: 10, color: '#DF177C' }}></i>
-                
-                                    <div className="flex flex-column align-items-center gap-1">
-                                        <span>Are you sure you want to delete selected rule. </span>
-                                        <span>This action cannot be undone. </span>
-                                    </div>
+                            header="Delete confirmation"
+                            visible={isDeleteDialogVisible}
+                            style={{ width: layoutState.isMobile ? '90vw' : '35vw' }}
+                            className="delete-dialog"
+                            footer={
+                                <div className="flex justify-content-center p-2">
+                                    <Button label="Cancel" style={{ color: '#DF177C' }} className="px-7" text onClick={closeDeleteDialog} />
+                                    <Button label="Delete" style={{ backgroundColor: '#DF177C', border: 'none' }} className="px-7 hover:text-white" onClick={onDelete} />
                                 </div>
-                            </Dialog>
-                            <Dialog
+                            }
+                            onHide={closeDeleteDialog}
+                        >
+                            {isLoading && (
+                                <div className="center-pos">
+                                    <ProgressSpinner style={{ width: '50px', height: '50px' }} />
+                                </div>
+                            )}
+                            <div className="flex flex-column w-full surface-border p-3 text-center gap-4">
+                                <i className="pi pi-info-circle text-6xl" style={{ marginRight: 10, color: '#DF177C' }}></i>
+
+                                <div className="flex flex-column align-items-center gap-1">
+                                    <span>
+                                        {isAllDeleteDialogVisible 
+                                            ? "Are you sure you want to delete all rule." 
+                                            : "Are you sure you want to delete selected rule."}
+                                    </span>
+                                    <span>This action cannot be undone. </span>
+                                </div>
+                            </div>
+                        </Dialog>
+
+                            {/* <Dialog
                                 header="Delete confirmation"
                                 visible={isAllDeleteDialogVisible}
                                 style={{ width: layoutState.isMobile ? '90vw' : '35vw' }}
@@ -583,7 +604,7 @@ const ManageRulesPage = () => {
                                         <span>This action cannot be undone. </span>
                                     </div>
                                 </div>
-                            </Dialog>
+                            </Dialog> */}
             </div>
         </div>
     );
